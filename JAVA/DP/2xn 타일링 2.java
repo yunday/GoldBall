08f092d1f56af4dd6250ca37161d1eb6f6ea8e5c
윤깅 <52197401+yunday@users.noreply.github.com>
2022-07-18 17:42:08 +0900

    Create 2xn 타일링 2.java

---
+++ JAVA/DP/2xn 타일링 2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 3;
+        for(int i=3;i<=n;i++){
+            dp[i] = ((dp[i-2]*2)%10007+dp[i-1])%10007;
+        }
+        System.out.println(dp[n]%10007);
+    }
+}
