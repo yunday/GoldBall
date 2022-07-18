ecbb7796e9e50cc14a89d12bb09ef077a91c15c4
윤깅 <52197401+yunday@users.noreply.github.com>
2022-07-18 17:49:30 +0900

    Update 2xn 타일링 2.java

--- JAVA/DP/2xn 타일링 2.java
+++ JAVA/DP/2xn 타일링 2.java
@@ -9,7 +9,7 @@ public class Main {
         
         int[] dp = new int[n+1];
         dp[1] = 1;
-        dp[2] = 3;
+        if(n>1) dp[2] = 3;
         for(int i=3;i<=n;i++){
             dp[i] = ((dp[i-2]*2)%10007+dp[i-1])%10007;
         }
